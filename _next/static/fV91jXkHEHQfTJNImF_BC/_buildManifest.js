ce8f0f2a181de2451c0a067755805fc477847b82
xHyroM <generalkubo@gmail.com>
2022-03-05 14:17:58 +0000

    Deploying to gh-pages from @ Mr-Infinity-Bot/status@ba7aa1c22d46ef708a99878ca4cf230eba5732d5 🚀

---
+++ _next/static/fV91jXkHEHQfTJNImF_BC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7bdd93f5150f4c47.css","static/chunks/pages/index-103f04f4b6285d79.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
